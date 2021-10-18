fa00ddba687dea2dd39ab106143ab54882623204
morgunovdv <morgunovdv3@hotmail.com>
2021-10-18 11:21:50 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Subtask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк, которые хотите использовать");
+            int n = int.Parse(Console.ReadLine());
+            string[] vs = new string[n];
+            string input2 = null;
+            while (input2 != "X")
+            {
+                input2 = Console.ReadLine();
+                for (int i = 0; i< vs.Length; i++)
+                {
+                    vs[i] = input2;
+                }
+            }
+
+            Console.WriteLine("=========================");
+
+            for (int i = 0; i < vs.Length; i++)
+            {
+                Console.WriteLine(vs[i]);
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
